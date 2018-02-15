8e89136fcdaa3f1837dc6528de92cbc6803c0239
asier <asier@192.168.0.16>
2018-02-15 17:19:28 +0100

    cambios totales definitivos

--- src/excepciones/CalculadoraMain.java
+++ src/excepciones/CalculadoraMain.java
@@ -1,5 +1,9 @@
 package excepciones;
 
+
+
+
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,46 +13,33 @@ public class CalculadoraMain extends Divisiones {
 	
 	
 	public static void main(String[] args) throws Exception {
-
+		float resultado = -1;
+		
 		Scanner scan = new Scanner (System.in);
 		Divisiones division1 = new Divisiones();
 		
-		division1.dividir();
 		
+		//division1.solicitaDatos();
+		division1.setDividendo(30);
+		division1.setDivisor(10);
 		
-//		String dividendo_str;
-//		String divisor_str;
-//		Integer dividendo;
-//		Integer divisor;
-//		float division = .0f;
-//		try{
-//			
-//			System.out.println("inserta numerador --> ");
-//			dividendo_str = scan.nextLine();
-//			
-//			System.out.println("inserta denominador --> ");
-//			divisor_str = scan.nextLine();
-//			
-//			
-//			dividendo = Integer.parseInt(dividendo_str);
-//			divisor = Integer.parseInt(divisor_str);
-//			
-//			division = (dividendo / divisor);
-//			
-//		} catch (ArithmeticException e) {
-//			//Cuando hay divisor =  0
-//			System.err.println("ERROR --> No se puede dividir entre 0 estas loco? " );   
-//		} catch (NullPointerException ex){
-//			//Cuando hay un  un valor vacio
-//			System.err.println("ERROR --> No puedes dejar ningun campo vacio "); 
-//		} catch(NumberFormatException ex){
-//			// Si introduces una frase en la calculadora 
-//			System.err.println("ERROR --> Por favor introduce un numero, no lo escribas "); 		
-//		}finally{
-//			System.out.println("Resultado: " + division);
-//		}
+		try {
+			resultado = division1.dividir();
+		} catch (ArithmeticException e) {
+			//Cuando hay divisor =  0
+			System.err.println("ERROR --> No se puede dividir entre 0 estas loco? " );   
+		} catch (NullPointerException ex){
+			//Cuando hay un  un valor vacio
+			System.err.println("ERROR --> No puedes dejar ningun campo vacio "); 
+		} catch(NumberFormatException ex){
+			// Si introduces una frase en la calculadora 
+			System.err.println("ERROR --> Por favor introduce un numero, no lo escribas "); 		
+	
 		
-		
+		division1.visualizarDatos(); //System.out.println("el resultado es " + resultado);
 	}
-
+		}
 }
+	
+		
+		
--- src/excepciones/Divisiones.java
+++ src/excepciones/Divisiones.java
@@ -1,46 +1,82 @@
 package excepciones;
 
+
 import java.util.Scanner;
 
 public class Divisiones {
 
+	private Integer dividendo = -1;
+	private Integer divisor = -1;
+	private float division = .0f;
 	
-	public void dividir() {
-		Scanner scan = new Scanner(System.in);
-		
-		String dividendo_str;
-		String divisor_str;
-		Integer dividendo;
-		Integer divisor;
-		float division = .0f;
-		
-			
-		
-			try{
-			System.out.println("inserta numerador --> ");
-			dividendo_str = scan.nextLine();
-			
-			System.out.println("inserta denominador --> ");
-			divisor_str = scan.nextLine();
-			
-			
-			dividendo = Integer.parseInt(dividendo_str);
-			divisor = Integer.parseInt(divisor_str);
-			
-			division = (dividendo / divisor);
-			
-	} catch (ArithmeticException e) {
-		//Cuando hay divisor =  0
-		System.err.println("ERROR --> No se puede dividir entre 0 estas loco? " );   
-	} catch (NullPointerException ex){
-		//Cuando hay un  un valor vacio
-		System.err.println("ERROR --> No puedes dejar ningun campo vacio "); 
-	} catch(NumberFormatException ex){
-		// Si introduces una frase en la calculadora 
-		System.err.println("ERROR --> Por favor introduce un numero, no lo escribas "); 		
-	}finally{
-		System.out.println("Resultado: " + division);
+	
+	
+	public void setDivisor(Integer _divisor) {
+		this.divisor = _divisor;
 	}
+
+	public Integer getDivisor() {
+		return this.divisor;
+	}
+
+
+	public void setDividendo(Integer _dividendo) {
+		this.dividendo = _dividendo;
+	}
+
+	public Integer getDividendo() {
+		return this.dividendo;
+	}
+
+  public void solicitaDatos() {
+	  
+
+		 String divisor_str = null;
+		 String dividendo_str = null;
+		
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("inserta numerador --> ");
+		dividendo_str = scan.nextLine();
+		
+		System.out.println("inserta denominador --> ");
+		divisor_str = scan.nextLine();
+		
+		
+		int divndo = Integer.parseInt(dividendo_str);
+		this.setDividendo(divndo);
+		int divor = Integer.parseInt(divisor_str);
+		this.setDivisor(divor);
+		
+	
+  }
+	public float dividir() {
+		
+			
+			this.solicitaDatos();
+			
+			return division = (this.getDividendo() / this.getDividendo());
+			
+	
+			/*		
+			} catch (ArithmeticException e) {
+				//Cuando hay divisor =  0
+				System.err.println("ERROR --> No se puede dividir entre 0 estas loco? " );   
+			} catch (NullPointerException ex){
+				//Cuando hay un  un valor vacio
+				System.err.println("ERROR --> No puedes dejar ningun campo vacio "); 
+			} catch(NumberFormatException ex){
+				// Si introduces una frase en la calculadora 
+				System.err.println("ERROR --> Por favor introduce un numero, no lo escribas "); 		
+			}finally{
+				System.out.println("Resultado: " + division);
+			}
+			*/
+	}
+
+	public void visualizarDatos() {
+		System.out.println("el resultado es " + division);
 		
 	}
+
+	
 }
